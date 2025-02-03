cc9d84ff46992c9c52e9c42e583130f9d06160b6
sarujan uthayachandran <sarujan_uthayachandran@sluz.ch>
2025-02-03 10:28:03 +0100

    WPF project initialized

---
+++ helveticride/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace helveticride
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+  }
+
+}
