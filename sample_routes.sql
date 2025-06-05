4b5c1602d1986473825813c4687470d63102a7fb
Saru <168628180+Saru0505@users.noreply.github.com>
2025-06-05 16:07:20 +0200

    Add files via upload
    save in \bin\Debug\net8.0-windows

---
+++ sample_routes.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO Routes (Start, End, Waypoints, IsFavorite, CreatedAt, Distance, Duration)
+VALUES
+  ('Andermatt', 'Sustenpass', 'Göschenen;Wassen;Meiental', 1, '2025-06-04 08:00:00', '45km', '1:00'),
+  ('Chur', 'San Bernardino', 'Thusis;Hinterrhein', 1, '2025-06-03 09:30:00', '75km', '1:20'),
+  ('Interlaken', 'Grimselpass', 'Meiringen;Innertkirchen', 0, '2025-06-02 13:15:00', '55km', '0:55'),
+  ('Locarno', 'Lukmanierpass', 'Biasca;Disentis', 0, '2025-06-01 10:00:00', '120km', '2:00'),
+  ('Montreux', 'Col de la Croix', 'Aigle;Les Diablerets', 1, '2025-05-31 14:30:00', '60km', '1:10'),
+  ('St. Gallen', 'Appenzell', 'Teufen;Gais', 0, '2025-06-01 16:00:00', '25km', '0:35');
